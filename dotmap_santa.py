20b3f49314a7fff58ab4b213ce7d7c01306af8dd
brunj7 <julien@Juliens-MacBookPro.local>
2014-11-17 23:21:06 -0800

    with postgis access as a class

--- dotmap_santa.py
+++ dotmap_santa.py
@@ -11,7 +11,7 @@ Original visualization: http://www.coopercenter.org/demographics/Racial-Dot-Map
 import sys
 import psycopg2
 from random import uniform
-#import matplotlib.pyplot as plt
+
 
 
 #Constants
@@ -26,49 +26,68 @@ SRID = 4269
 ratio_pop = 100
 
 #====================================================
- 
-def query_postgis(sql_string, data=None):
-    '''query a postgis table'''
-    #Create the cursor
-    cur = conn.cursor()
-    # execute the query
-    if data:
-        cur.execute(sql_string, data)
-    else:
-        cur.execute(sql_string)
-    # store the result of the query into Tuple
-    rows = cur.fetchall()
-    # closes the connection
-    cur.close()
-    #return the data
-    return rows
 
-def create_table(field_tuple):
-    '''creates a postgis table'''
-    #Create the query
-    sql_create = "CREATE TABLE %s.%s (" %(scheme_name, output_table_name)
-    sql_fields = ", ".join(field_tuple)
-    sql_owner = ") WITH (OIDS=FALSE); ALTER TABLE public.%s OWNER TO julien; CREATE INDEX ind_geom_point ON public.tracts_sb_dots USING gist(geom);" %(output_table_name)
-    sql_query = ''.join([sql_create,sql_fields,sql_owner])
-    #Execute the query
-    cur = conn.cursor()
-    try:
-        cur.execute(sql_query)
-    except:
-        conn.rollback()
-        sql =  "DROP TABLE %s.%s" %(scheme_name, output_table_name)
-        cur.execute(sql);
-        cur.execute(sql_query)
-    conn.commit()
-    cur.close()
+class Postgis:
+    '''class allowing basic interactions (connection to db, table creation, query, table row insertion)'''
+    
+    def __init__(self, dbname, hostname, username, passwd):
+        '''initialize the connection'''
+        try:
+            self.conn = psycopg2.connect("dbname= %s host=%s user=%s password=%s" %(dbname, hostname, username, passwd))   
+        except:
+            print "I am unable to connect to the database"
+            sys.exit()
 
-def insert_many(value_dict):
-    '''insert a bulk of rows into a table'''  
-    cur = conn.cursor()
-    cur.executemany("""INSERT INTO tracts_SB_dots(geoid,namelsad,geom) VALUES (%(geoid)s, %(tract_name)s, ST_GeomFromText(%(geom)s, 4269))""", value_dict)
-    #cur.execute("INSERT INTO tracts_SB_dots(geoid,namelsad,geom) VALUES ('123','dede',ST_SetSRID(ST_MakePoint(-119.880634657, 34.4170923867),4269));")
-    conn.commit()
-    cur.close()
+        
+    def close_connection(self):
+        '''close the db connection'''
+        #close the connection
+        self.conn.close()
+
+    def query_postgis(self, sql_string, data=None):
+        '''query a postgis table'''
+        #Create the cursor
+        cur = self.conn.cursor()
+        # execute the query
+        if data:
+            cur.execute(sql_string, data)
+        else:
+            cur.execute(sql_string)
+        # store the result of the query into Tuple
+        rows = cur.fetchall()
+        # closes the connection
+        cur.close()
+        #return the data
+        return rows
+    
+    def create_table(self, scheme, output_table, fields):
+        '''creates a postgis table'''
+        #Create the query
+        sql_create = "CREATE TABLE %s.%s (" %(scheme, output_table)
+        sql_fields = ", ".join(fields)
+        sql_owner = ") WITH (OIDS=FALSE); ALTER TABLE public.%s OWNER TO julien; CREATE INDEX ind_geom_point ON public.tracts_sb_dots USING gist(geom);" %(output_table)
+        sql_query = ''.join([sql_create,sql_fields,sql_owner])
+        #Execute the query
+        cur = self.conn.cursor()
+        try:
+            cur.execute(sql_query)
+        except:
+            self.conn.rollback()
+            sql = "DROP TABLE %s.%s" %(scheme, output_table)
+            cur.execute(sql);
+            cur.execute(sql_query)
+        self.conn.commit()
+        cur.close()
+    
+    def insert_many(self, value_dict):
+        '''insert a bulk of rows into a table'''  
+        cur = self.conn.cursor()
+        cur.executemany("""INSERT INTO tracts_SB_dots(geoid,namelsad,geom) VALUES (%(geoid)s, %(tract_name)s, ST_GeomFromText(%(geom)s, 4269))""", value_dict)
+        #cur.execute("INSERT INTO tracts_SB_dots(geoid,namelsad,geom) VALUES ('123','dede',ST_SetSRID(ST_MakePoint(-119.880634657, 34.4170923867),4269));")
+        self.conn.commit()
+        cur.close()
+
+#====================================================        
 
 def point_generator(nbr_dots, lg_min, lg_max, la_min, la_max, polyg_id):
     '''generates random (lat,long) locations within a specific polygon''' 
@@ -86,7 +105,7 @@ def point_generator(nbr_dots, lg_min, lg_max, la_min, la_max, polyg_id):
             #SELECT ST_WITHIN(ST_GeomFromText('POINT(34.42 -119.82)', 4269),tracts_sb.geom) from tracts_sb WHERE geoid10 = '06083980000';
             data_test_in = {'lat':sample_point[0], 'lon':sample_point[1], 'SRID':SRID, 'table':table_name_in,'field_geom':"%s.geom" %(table_name_in), 'field_id': "geoid10", 'value_id': "'"+polyg_id+"'"}
             query = query_test_in % data_test_in
-            test = query_postgis(query)
+            test = pg_conn.query_postgis(query)
             if test[0][0] is True:
                 #Add the point location to the dictionary
                 point_dict[i] = "POINT(%s %s)" %(sample_point[0], sample_point[1])
@@ -98,22 +117,18 @@ def point_generator(nbr_dots, lg_min, lg_max, la_min, la_max, polyg_id):
     return point_dict
 
 
-#==========================================================================================================
+#++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
 if __name__=='__main__':
     # create connection to database
-    try:
-        conn = psycopg2.connect("dbname= %s host=%s user=%s password=%s" %(db_name, host, user_name, pwd))   
-    except:
-        print "I am unable to connect to the database"
-        exit
+    pg_conn = Postgis(db_name, host, user_name, pwd)
     
     # create output table
     fields = ('gid SERIAL PRIMARY KEY', 'geoid character varying(11)', 'namelsad character varying(20)', 'geom geometry(Point,4269)')
-    create_table(fields)
+    pg_conn.create_table(scheme_name, output_table_name, fields)
     
     query = "SELECT gid, geoid10, namelsad10, dp0010001/%s AS nbr_dots, BOX2D(geom) as BBOX from %s.%s;" %(ratio_pop, scheme_name, table_name_in)
-    data_all = query_postgis(query)
+    data_all = pg_conn.query_postgis(query)
     data_dict = {}
     for data in data_all:
         dict_row = {}
@@ -138,8 +153,9 @@ if __name__=='__main__':
                 data_insert.append(dict_row.copy())
             
             #Insert the rows into the PostGIS table
-            insert_many(tuple(data_insert))
+            pg_conn.insert_many(tuple(data_insert))
     
     #close the connection
-    conn.close()
-    print "All Done!"
+    pg_conn.close_connection()
+    
+    print "Dots table successfully created"
